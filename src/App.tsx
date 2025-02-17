698f497818ade13215e2a768ba24442b3cf57b66
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-02-17 18:47:07 -0500

    main and app component updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-function App() {
+import React from "react";
+import { motion, useScroll, useSpring } from "framer-motion";
+import Navbar from "./components/Navbar";
+const Hero = React.lazy(() => import("./components/Hero"));
+const Services = React.lazy(() => import("./components/Services"));
+const Partners = React.lazy(() => import("./components/Partners"));
+const About = React.lazy(() => import("./components/About"));
+const CallToAction = React.lazy(() => import("./components/CallToAction"));
+const Footer = React.lazy(() => import("./components/Footer"));
+import "./styles/globals.css";
+
+export default function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
     <>
-      <h1>Hello World</h1>
+      <motion.div className="scroll-progress" style={{ scaleX }} />
+
+      <Navbar />
+      <Hero />
+      <Services />
+      <About />
+      <Partners />
+      <CallToAction />
+      <Footer />
     </>
   );
 }
-
-export default App;
