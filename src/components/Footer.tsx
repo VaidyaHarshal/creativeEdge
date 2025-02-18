670bb68a51acbd02608cf509e84eb3c3ea04ba9a
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-02-18 18:07:31 -0500

    removed some redundant css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,4 @@
-import {
-  FaInstagram,
-  FaFacebook,
-  FaTwitter,
-  FaApple,
-  FaAndroid,
-} from "react-icons/fa";
+import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "../styles/footer.css";
 
@@ -17,6 +11,19 @@ export default function Footer() {
           {/* Brand Section */}
           <div className="footer-brand">
             <h4 className="footer-heading">Creative Edge</h4>
+            {/* Contact Section */}
+            <div className="contact-section">
+              <h4 className="contact-heading">Contact Us</h4>
+
+              <div className="contact-details">
+                <p>
+                  <strong>Address:</strong> Mumbai, Maharashtra, India
+                </p>
+                <p className="marketing-email">
+                  <strong>Email:</strong> marketing@creativeedge.com
+                </p>
+              </div>
+            </div>
             <div className="social-links">
               <a href="#" aria-label="Instagram">
                 <FaInstagram />
@@ -28,23 +35,6 @@ export default function Footer() {
                 <FaTwitter />
               </a>
             </div>
-            {/* Contact Section */}
-            <div className="contact-section">
-              <h4 className="contact-heading">Contact Us</h4>
-              <p className="contact-text">
-                Better yet, see us in person!
-                <br />
-                We love our customers, so feel free to contact us.
-              </p>
-              <div className="contact-details">
-                <p>
-                  <strong>📍 Address:</strong> Mumbai, Maharashtra, India
-                </p>
-                <p className="marketing-email">
-                  <strong>📧 Email:</strong> marketing@creativeedge.com
-                </p>
-              </div>
-            </div>
           </div>
           {/* Company */}
           <div className="footer-group">
@@ -53,22 +43,7 @@ export default function Footer() {
               <li>
                 <Link to="/about">About Us</Link>
               </li>
-              <li>
-                <Link to="/business">For Business</Link>
-              </li>
-              <li>
-                <Link to="/partners">Help Center</Link>
-              </li>
-              <li>
-                <Link to="/careers">Careers</Link>
-              </li>
-            </ul>
-          </div>
 
-          {/* Legal */}
-          <div className="footer-group">
-            <h4 className="footer-subheading">Legal</h4>
-            <ul className="footer-links">
               <li>
                 <a
                   href="https://policies.google.com/privacy"
@@ -87,28 +62,11 @@ export default function Footer() {
                   Terms of Service
                 </a>
               </li>
+              <li>
+                <Link to="/careers">Careers</Link>
+              </li>
             </ul>
           </div>
-
-          {/* Apps */}
-          <div className="footer-group">
-            <h4 className="footer-subheading">Get the App</h4>
-            <div className="app-links">
-              <a href="#" className="app-button">
-                <FaApple /> iOS App
-              </a>
-              <a href="#" className="app-button">
-                <FaAndroid /> Android App
-              </a>
-            </div>
-          </div>
-        </div>
-
-        {/* Account Links */}
-        <div className="account-links">
-          <Link to="/signup">Create Account</Link>
-          <span className="divider">|</span>
-          <Link to="/login">Sign In</Link>
         </div>
       </div>
 
